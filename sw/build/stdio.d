e4b0dd06a3b1bbc7667471e097cc04df7f9bcb7b
Federico Ramos <federico.g.ramos@gmail.com>
2025-05-01 21:24:11 -0300

    All pcb filenames to small.

--- sw/build/stdio.d
+++
@@ -1,1 +0,0 @@
-stdio.d stdio.p1: C:\fgr\16gb\0000 interior\deVolt\sw\stdio.c C:/Program\ Files\ (x86)/HI-TECH\ Software/PICC-18/9.80/include/stdarg.h C:/Program\ Files\ (x86)/HI-TECH\ Software/PICC-18/9.80/include/string.h C:/fgr/16gb/libSwPic/oldV2/lib_sw_pic_20171007/stdio.h
--- sw/timer_user_settings.h
+++
@@ -1,9 +0,0 @@
-// timerInt_aliases.h
-////////////////////////////////////////////////////////////////////////////////
-#ifndef __TIMER_USER_SETTINGS_H
-#define __TIMER_USER_SETTINGS_H
-
-// ALIAS
-typedef short long type_timerSize;
-
-#endif//__TIMER_INT_ALIASES_H
